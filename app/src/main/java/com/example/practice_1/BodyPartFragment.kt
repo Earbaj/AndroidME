558683dacf88bc297a1a6c69048a92f6af725f58
Earbaj <earbajsaria3@gmail.com>
2022-07-18 00:35:11 -0700

    Add onSaveInstanceState to save fragment current state to avoid loss data while rotation

--- app/src/main/java/com/example/practice_1/BodyPartFragment.kt
+++ app/src/main/java/com/example/practice_1/BodyPartFragment.kt
@@ -8,9 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import com.example.practice_1.AndroidImageAssets
+import java.util.ArrayList
 
 class BodyPartFragment() : Fragment() {
 
+    //Id's for onsavedInstanceState
+    var IMAGE_ID_LIST : String = "image_ids"
+    var LIST_INDEX : String = "list_index"
+
+    //Variable for image and listindex
     var mImageIds : List<Int>? = null
     var mListIndex : Int = 1
 
@@ -19,6 +25,12 @@ class BodyPartFragment() : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        if (savedInstanceState != null){
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_ID_LIST)
+            mListIndex = savedInstanceState.getInt(LIST_INDEX)
+        }
+
         val assets = AndroidImageAssets()
         var rootView = inflater.inflate(R.layout.fragment_body_part, container, false)
         var imageView = rootView.findViewById<ImageView>(R.id.body_part_image_view)
@@ -26,7 +38,12 @@ class BodyPartFragment() : Fragment() {
         if (mImageIds != null){
             imageView.setImageResource(mImageIds!!.get(mListIndex))
             imageView.setOnClickListener {
-
+                if(mListIndex < mImageIds!!.size-1){
+                    mListIndex++
+                }else{
+                    mListIndex = 1
+                }
+                imageView.setImageResource(mImageIds!!.get(mListIndex))
             }
         }else {
             Log.v("TAG", "This Fragment has null list of id's")
@@ -34,10 +51,17 @@ class BodyPartFragment() : Fragment() {
         return rootView
     }
 
+    //Setters for Image ids and index
     fun setImageIds(imageIds : List<Int>){
         mImageIds = imageIds
     }
     fun setListIndex(index : Int){
         mListIndex = index
+    }
+
+    //Save the current state of fragment
+    override fun onSaveInstanceState(currentState: Bundle) {
+        currentState.putIntegerArrayList(IMAGE_ID_LIST, mImageIds as ArrayList<Int>?)
+        currentState.putInt(LIST_INDEX, mListIndex)
     }
 }
--- app/src/main/java/com/example/practice_1/MainActivity.kt
+++ app/src/main/java/com/example/practice_1/MainActivity.kt
@@ -8,32 +8,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Head Fragment
-        val headFragment = BodyPartFragment()
-        val androidImageAssets = AndroidImageAssets()
-        headFragment.setImageIds(androidImageAssets.getHeads())
-        headFragment.setListIndex(6)
+        if(savedInstanceState == null){
+            //Head Fragment
+            val headFragment = BodyPartFragment()
+            val androidImageAssets = AndroidImageAssets()
+            headFragment.setImageIds(androidImageAssets.getHeads())
+            headFragment.setListIndex(6)
 
-        var fragmentManager = supportFragmentManager
+            var fragmentManager = supportFragmentManager
 
-        fragmentManager.beginTransaction()
-            .add(R.id.head_container, headFragment)
-            .commit()
+            fragmentManager.beginTransaction()
+                .add(R.id.head_container, headFragment)
+                .commit()
 
-        //Body Fragment
-        val bodyFragment = BodyPartFragment()
-        bodyFragment.setImageIds(androidImageAssets.getBody())
-        bodyFragment.setListIndex(2)
-        fragmentManager.beginTransaction().
-        add(R.id.body_container, bodyFragment).
-        commit()
+            //Body Fragment
+            val bodyFragment = BodyPartFragment()
+            bodyFragment.setImageIds(androidImageAssets.getBody())
+            bodyFragment.setListIndex(2)
+            fragmentManager.beginTransaction().
+            add(R.id.body_container, bodyFragment).
+            commit()
 
-        //Leg Fargment
-        val legFragment = BodyPartFragment()
-        legFragment.setImageIds(androidImageAssets.getLeg())
-        legFragment.setListIndex(2)
-        fragmentManager.beginTransaction().
-        add(R.id.leg_container, legFragment).
-        commit()
+            //Leg Fargment
+            val legFragment = BodyPartFragment()
+            legFragment.setImageIds(androidImageAssets.getLeg())
+            legFragment.setListIndex(2)
+            fragmentManager.beginTransaction().
+            add(R.id.leg_container, legFragment).
+            commit()
+        }
+
     }
 }
